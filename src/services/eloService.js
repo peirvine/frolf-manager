9e4f8f41c8ae05aeef64ca3b34e39d9f910178c6
Peter Irvine <peirvine@gmail.com>
2023-08-16 21:34:25 -0500

    basic elo math

--- src/services/eloService.js
+++ src/services/eloService.js
@@ -2,7 +2,7 @@
 import { writeEloTracking, getELOHistory, updateCurrentElo, getCurrentElo } from "../firebase";
 
 
-export const calculateElo = (card) => {
+export const calculateElo = async (card) => {
   const parLocation = card.playerArray.map((e) => { return e.player; }).indexOf("Par")
   const playerArray = card.playerArray
   const parCard = parLocation === 0 ? playerArray.shift() : playerArray.splice(0, parLocation)
@@ -10,11 +10,42 @@ export const calculateElo = (card) => {
   const strokesPerHole = getStrokesPerHole(playerArray, cardAverage)
   // const players = formatPlayers(players)
   const pointsPerHole = 5
-  const averageEloOfPlayers = getAverageEloOFPlayers(card.playerArray)
+  let averageEloOfPlayers = await getAverageEloOFPlayers(card.playerArray)
   const formattedPlayers = formatPlayers()
+  const pointsPerThrow = calculatePointsPerThrow()
+  calculateCardElo(playerArray, pointsPerThrow, cardAverage, averageEloOfPlayers)
   const prettyElo = makeEloPretty(card.course, card.layout, cardAverage, strokesPerHole, pointsPerHole, averageEloOfPlayers, formattedPlayers)
   // updateCurrentElo(prettyElo)
   // writeEloTracking(prettyElo)
+}
+
+// TODO: Remove the need for this. This method will be impossible for multiple leagues, but MAFTB it's fine
+const translateUsers = player => {
+  console.warn('player', player)
+  if ("alex oelke".match(player.toLowerCase())) {
+    return "alex"
+  }
+  if ("benton campbell".match(player.toLowerCase())) {
+    return "benton"
+  }
+  if ("greg ledray".match(player.toLowerCase())) {
+    return "greg"
+  }
+  if ("jimmy donadio".match(player.toLowerCase())) {
+    return "jimmy"
+  }
+  if ("lane scherber".match(player.toLowerCase())) {
+    return "lane"
+  }
+  if ("peter irvine".match(player.toLowerCase())) {
+    return "peter"
+  }
+  if ("robert renkor".match(player.toLowerCase()) || "rob renkor".match(player.toLowerCase())) {
+    return "rob"
+  }
+  if ("samir ramakrishnan".match(player.toLowerCase())) {
+    return "samir"
+    }
 }
 
 const getCardAverage = (card) => {
@@ -44,12 +75,26 @@ const historicalEloAsync = () => {
 const getAverageEloOFPlayers = async (players) => {
   const elos = await currentEloAsync()
   let presentPlayers = []
-  players.map(player => presentPlayers.push(player.player))
+  players.map(player => presentPlayers.push(translateUsers(player.player)))
   let sumElo = 0
   presentPlayers.map(player => {
     sumElo += elos[player.toLowerCase()]
   })
   return sumElo / presentPlayers.length
+}
+
+const calculatePointsPerThrow = () => {
+  return 7.98230623
+}
+
+const calculatePlayerElo = (groupAverage, playerScore, pointsPerThrow, averageEloOfPlayers) => {
+  console.warn('elo calc', ((groupAverage - playerScore) * pointsPerThrow) + averageEloOfPlayers)
+}
+
+const calculateCardElo = (players, pointsPerThrow, cardAverage, averageEloOfPlayers) => {
+  players.map(player => {
+    calculatePlayerElo(84.9, player.total, pointsPerThrow, averageEloOfPlayers)
+  })
 }
 
 const formatPlayers = () => {
