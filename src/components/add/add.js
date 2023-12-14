b6c7a95d1009b3dfa2c46310d863ccba0312686e
Peter Irvine <peirvine@gmail.com>
2023-12-14 11:18:22 -0600

    more user experience upgrades

--- src/components/add/add.js
+++ src/components/add/add.js
@@ -1,13 +1,17 @@
+/* eslint-disable no-unused-vars */
 import React, { useState } from 'react';
 import { Autocomplete, Button, FormControl, TextField, Alert, Collapse, Box, Grid, Accordion, AccordionSummary, AccordionDetails } from '@mui/material'
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { NavLink } from "react-router-dom";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth, signInWithGoogle } from "../../firebase"
 import { addScorecard, uDiscDump } from '../../services/scorecardService';
 
 import './add.scss'
 
 export default function Add() {
   const [layouts, setLayouts] = useState([])
+  const [user] = useAuthState(auth);
 
   const [chosenCourse, setChosenCourse] = useState('')
   const [chosenLayout, setChosenLayout] = useState('')
@@ -16,6 +20,7 @@ export default function Add() {
   const [open, setOpen] = useState(false)
   const [variant, setVariant] = useState('info')
   const [alertMessage, setAlertMessage] = useState('')
+  
   let playerScores = {
     Alex: '',
     Benton: '',
@@ -80,102 +85,113 @@ export default function Add() {
     playerScores[player] = value
   }
 
+  console.warn('user', user)
+
   return (
     <div className="addScorecard">
       <h1>Add a Scorecard</h1>
-      <Collapse in={open}>
-        <Alert severity={variant} onClose={() => setOpen(false)}>
-          {alertMessage}
-        </Alert>
-      </Collapse>
+      {user ? (
+        <>
+          <Collapse in={open}>
+            <Alert severity={variant} onClose={() => setOpen(false)}>
+              {alertMessage}
+            </Alert>
+          </Collapse>
 
-      <Box sx={{ width: '100%', flexGrow: 1 }}>
-        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-          <Grid item md={6} xs={12}>
-            <h2>Automatic UDisc Import</h2>
-            <p>To do this go to your card in UDisc, click the Hamburger Menu, click "Export to CSV", then copy the data and paste it into the field below. This is the preferred way to import data.</p>
-            <FormControl>
-              <TextField
-                required
-                id="outlined-basic"
-                label="Udisc CSV Output"
-                variant="outlined"
-                value={udisc}
-                onInput={e => setUdisc(e.target.value)}
-              />
-              <Button className="submitButton" variant="contained" onClick={() => handleUdisc()}>Parse UDisc CSV</Button>
-              <Button className="submitButton" variant="outlined" component={ NavLink } to={"/rankings"}>View Rankings</Button>
-            </FormControl>
-          </Grid>
-          <Grid item md={6} xs={12}>
-            <h2>Manual Add</h2>
-            <p>This is for rounds not done on UDisc, this will limit the amount of data/stats that players will have for the round</p>
-            <FormControl>
-              <Autocomplete
-                disablePortal
-                freeSolo
-                required
-                onChange={(event, newValue) => {
-                  setLayouts(newValue.layouts)
-                  setChosenCourse(newValue.label)
-                }}
-                id="frolfCourse"
-                options={frolfCourses}
-                value={chosenCourse}
-                renderInput={(params) => <TextField {...params} label="Course" />}
-              />
-              <Autocomplete
-                className="formItem"
-                disablePortal
-                freeSolo
-                required
-                // eslint-disable-next-line no-unused-vars
-                onChange={(event, newValue) => {
-                  setChosenLayout(newValue)
-                }}
-                id="frolfLayout"
-                options={layouts}
-                value={chosenLayout}
-                renderInput={(params) => <TextField {...params} label="Layout" />}
-              />
-              <TextField
-                className="formItem"
-                id="outlined-basic"
-                label="Par"
-                required
-                variant="outlined"
-                value={par}
-                onInput={e => setPar(e.target.value)}
-              />
-              <Accordion className="formItem">
-                <AccordionSummary
-                  expandIcon={<ExpandMoreIcon />}
-                  aria-controls="panel1a-content"
-                  id="panel1a-header"
-                >
-                  Players
-                </AccordionSummary>
-                <AccordionDetails>
-                  <div className="players">
-                    Enter the players score
-                    {players.map(player => (
-                      <TextField
-                        key={player}
-                        className="formItem"
-                        id="outlined-basic"
-                        label={player}
-                        variant="outlined"
-                        onInput={e => handleManualAdd(e.target.value, player)}
-                      />
-                    ))}
-                  </div>
-                </AccordionDetails>
-              </Accordion>
-              <Button className="submitButton" variant="contained" onClick={() => handleSubmit()}>Add Scorecard</Button>
-            </FormControl>
-          </Grid>
-        </Grid>
-      </Box>
+          <Box sx={{ width: '100%', flexGrow: 1 }}>
+            <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+              <Grid item md={6} xs={12}>
+                <h2>Automatic UDisc Import</h2>
+                <p>To do this go to your card in UDisc, click the Hamburger Menu, click "Export to CSV", then copy the data and paste it into the field below. This is the preferred way to import data.</p>
+                <FormControl>
+                  <TextField
+                    required
+                    id="outlined-basic"
+                    label="Udisc CSV Output"
+                    variant="outlined"
+                    value={udisc}
+                    onInput={e => setUdisc(e.target.value)}
+                  />
+                  <Button className="submitButton" variant="contained" onClick={() => handleUdisc()}>Parse UDisc CSV</Button>
+                  <Button className="submitButton" variant="outlined" component={ NavLink } to={"/rankings"}>View Rankings</Button>
+                </FormControl>
+              </Grid>
+              <Grid item md={6} xs={12}>
+                <h2>Manual Add</h2>
+                <p><i>This feature is in development</i></p>
+                {/* <p>This is for rounds not done on UDisc, this will limit the amount of data/stats that players will have for the round</p>
+                <FormControl>
+                  <Autocomplete
+                    disablePortal
+                    freeSolo
+                    required
+                    onChange={(event, newValue) => {
+                      setLayouts(newValue.layouts)
+                      setChosenCourse(newValue.label)
+                    }}
+                    id="frolfCourse"
+                    options={frolfCourses}
+                    value={chosenCourse}
+                    renderInput={(params) => <TextField {...params} label="Course" />}
+                  />
+                  <Autocomplete
+                    className="formItem"
+                    disablePortal
+                    freeSolo
+                    required
+                    // eslint-disable-next-line no-unused-vars
+                    onChange={(event, newValue) => {
+                      setChosenLayout(newValue)
+                    }}
+                    id="frolfLayout"
+                    options={layouts}
+                    value={chosenLayout}
+                    renderInput={(params) => <TextField {...params} label="Layout" />}
+                  />
+                  <TextField
+                    className="formItem"
+                    id="outlined-basic"
+                    label="Par"
+                    required
+                    variant="outlined"
+                    value={par}
+                    onInput={e => setPar(e.target.value)}
+                  />
+                  <Accordion className="formItem">
+                    <AccordionSummary
+                      expandIcon={<ExpandMoreIcon />}
+                      aria-controls="panel1a-content"
+                      id="panel1a-header"
+                    >
+                      Players
+                    </AccordionSummary>
+                    <AccordionDetails>
+                      <div className="players">
+                        Enter the players score
+                        {players.map(player => (
+                          <TextField
+                            key={player}
+                            className="formItem"
+                            id="outlined-basic"
+                            label={player}
+                            variant="outlined"
+                            onInput={e => handleManualAdd(e.target.value, player)}
+                          />
+                        ))}
+                      </div>
+                    </AccordionDetails>
+                  </Accordion>
+                  <Button className="submitButton" variant="contained" onClick={() => handleSubmit()}>Add Scorecard</Button>
+                </FormControl> */}
+              </Grid>
+            </Grid>
+          </Box>
+        </>
+        ) : (
+          <>
+            <p>Please <span style={{ textDecoration: "underline" }}onClick={() => signInWithGoogle()}>log in</span> to add a round</p>
+          </>
+        )}
     </div>
   )
 }
--- src/components/scorecards/scorecards.js
+++ src/components/scorecards/scorecards.js
@@ -9,8 +9,10 @@ import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import './scorecards.scss'
 
 export default function ViewScorecards () {
+  const year = new Date().getFullYear();
   const [dataV2, setDataV2] = useState([])
   const [expanded, setExpanded] = useState(false);
+  const [yearExpanded, setYearExpanded] = useState(year.toString())
 
   useEffect(() => { 
     const a = getScorecards()
@@ -33,11 +35,15 @@ export default function ViewScorecards () {
     res.map(x => {
       sorted[x.Date.substring(0,4)].push(x)
     })
-    setDataV2(sorted)
+    setDataV2(sorted.sort((a, b) =>  (a < b) ? 1 : ((b < a) ? -1 : 0)))
   }
 
   const handleChange = (panel) => (event, isExpanded) => {
     setExpanded(isExpanded ? panel : false);
+  };
+
+  const handleYearChange = (panel) => (event, isExpanded) => {
+    setYearExpanded(isExpanded ? panel : false);
   };
 
   const formatDate = (date) => {
@@ -60,41 +66,50 @@ export default function ViewScorecards () {
       {dataV2.length !== 0 ? (dataV2.map(year => {
         year.sort((a,b) => (formatDate(a.Date) > formatDate(b.Date)) ? 1 : ((formatDate(b.Date) > formatDate(a.Date)) ? -1 : 0))
         return (
-          <div className="test">
-            <h2>{year[0].Date.slice(0, 4)}</h2>
-            {
-              year.map(round => {
-                const coursePar = getCoursePar(round.Players)
-                const sortedCard = sortCard(round.Players, coursePar)
-                return (
-                  <Accordion expanded={expanded === round.id} onChange={handleChange(round.id)}>
-                    <AccordionSummary
-                      expandIcon={<ExpandMoreIcon />}
-                      aria-controls="panel1a-content"
-                      id="panel1a-header"
-                    >
-                      <p><strong>{round.Course}</strong>: {round.Layout} - {round.Date.slice(0, round.Date.indexOf(" "))}</p>
-                    </AccordionSummary>
-                    <AccordionDetails>
-                      <TableContainer>
-                        <Table>
-                          <TableBody>
-                            {sortedCard.map((y) => (
-                              <ScorecardTable card={y} par={round.Par} coursePar={coursePar} />
-                            ))}
-                          </TableBody>
-                        </Table>
-                      </TableContainer>
-                    </AccordionDetails>
-                  </Accordion>
-                )
-              })
-            }
-          </div>
+          <Accordion expanded={yearExpanded === year[0].Date.slice(0, 4)} onChange={handleYearChange(year[0].Date.slice(0, 4))}>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+               <h2>{year[0].Date.slice(0, 4)}</h2>
+            </AccordionSummary>
+            <AccordionDetails>
+              <div className="test">
+                {
+                  year.map(round => {
+                    const coursePar = getCoursePar(round.Players)
+                    const sortedCard = sortCard(round.Players, coursePar)
+                    return (
+                      <Accordion expanded={expanded === round.id} onChange={handleChange(round.id)}>
+                        <AccordionSummary
+                          expandIcon={<ExpandMoreIcon />}
+                          aria-controls="panel1a-content"
+                          id="panel1a-header"
+                        >
+                          <p><strong>{round.Course}</strong>: {round.Layout} - {round.Date.slice(0, round.Date.indexOf(" "))}</p>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                          <TableContainer>
+                            <Table>
+                              <TableBody>
+                                {sortedCard.map((y) => (
+                                  <ScorecardTable card={y} par={round.Par} coursePar={coursePar} />
+                                ))}
+                              </TableBody>
+                            </Table>
+                          </TableContainer>
+                        </AccordionDetails>
+                      </Accordion>
+                    )
+                  })
+                }
+              </div>
+            </AccordionDetails>
+          </Accordion>
         )
       })) : (
         <div className="skeletor">
-          <Skeleton variant="text" width={100} sx={{ fontSize: '1.5em' }} className="individualSkeletor" />
           <Skeleton variant="rounded" animation="wave" height={60} className="individualSkeletor" />
           <Skeleton variant="rounded" animation="wave" height={60} className="individualSkeletor" />
           <Skeleton variant="rounded" animation="wave" height={60} className="individualSkeletor" />
