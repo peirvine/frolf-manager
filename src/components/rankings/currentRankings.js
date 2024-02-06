35f60f41161caf82330fabffbbd7a4d906405aae
Peter Irvine <peirvine@gmail.com>
2024-02-06 09:28:05 -0600

    Update currentRankings.js

--- src/components/rankings/currentRankings.js
+++ src/components/rankings/currentRankings.js
@@ -241,7 +241,7 @@ export default function CurrentRankings () {
           disableClearable
           id="combo-box-demo"
           options={optionArray}
-          sx={{ marginTop: 1, width: "25%" }}
+          sx={{ marginTop: 1, marginBottom: 1 }}
           renderInput={(params) => <TextField {...params} label="Choose League" defaultValue={params[0]}/>}
           onChange={(event, newValue) => {
             handleChangeLeague(newValue);
